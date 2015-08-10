a0b0be943f2d8e0af6ace10f5c67ddc6a78ba3b1
Alex Gavrishev <alex.gavrishev@gmail.com>
2015-08-10 23:05:19 +0300

    order class

---
+++ EtbApi/Model/Order.swift
@@ -0,0 +1,33 @@
+//
+//  Order.swift
+//  Hotels
+//
+//  Created by Alex Gavrishev on 8/10/15.
+//  Copyright © 2015 Alex Gavrishev. All rights reserved.
+//
+
+import Foundation
+
+class Order: NSObject, ResponseObjectSerializable {
+    var orderId: Int!
+    var personal: Personal!
+    var pyment: Payment!
+    var rates: [OrderRate]!
+    
+    required init(response: NSHTTPURLResponse, representation: AnyObject) {
+     // TODO
+    }
+    
+    class OrderRate {
+        var accommodationId: Int!
+        var rateId: String!
+        var statusCode: String!
+        var confirmationId: String!
+        var name: String!
+        var rateCount: Int!
+        var checkIn: String!
+        var checkOut: String!
+    }
+
+    
+}
